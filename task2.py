86a6f9c438f15d37680cf2e59c182693aa73aa51
Pranav <pranavmpv@gmail.com>
2020-04-05 13:57:50 -0700

    making it faster v1

--- task2.py
+++ task2.py
@@ -12,44 +12,11 @@ start_time = time()
 # ----------------------------------------------------------------------------------------------------------------------
 
 def get_network_components(old_components, edge_to_break):
-    '''
-    :param old_network_components: calculate network components and send - dict of adjlist
-    :param edge_to_break: edge to break and then calc network components
-    :return: new_net_comps - list of RDDs
-    '''
-    # print("(get_network_components) Old components [find new comps with these]", old_components)
 
-    # print("(get_network_components) Edge to break:", edge_to_break)
-    old_network_components = []
-    for compnt in old_components:
-        old_network_components.append(compnt.collectAsMap())
+    old_network_components = [comp.collectAsMap() for comp in old_components]
 
     new_components = []
-    if edge_to_break is None:
-        for component in old_network_components:
-            # perform bfs and keep adding RDDs of new components to new_components
-            vertices = set(component.keys())
-            visited = set()
-            # new_components =[]
-            remaining_vertices = set(component.keys())
-            while remaining_vertices:
-                bfsQ = [choice(tuple(remaining_vertices))]
-                new_component = [(bfsQ[0], component[bfsQ[0]])]
-                while bfsQ:
-                    node = bfsQ.pop(0)
-                    visited.add(node)
-                    children = component[node]
-                    for child in children:
-                        if child not in visited:
-                            visited.add(child)
-                            bfsQ.append(child)
-                            if (node, children) not in new_component:
-                                new_component.append((node, children))
-                            if (child, component[child]) not in new_component:
-                                new_component.append((child, component[child]))
-                new_components.append(sc.parallelize(new_component))
-                remaining_vertices = vertices.difference(visited)
-    else:
+    if edge_to_break is not None:
         # removing that edge
         for component in old_network_components:
             if edge_to_break[0] in component.keys():
@@ -58,43 +25,38 @@ def get_network_components(old_components, edge_to_break):
                 # print("After removing - ", component[edge_to_break[0]])
                 if len(component[edge_to_break[0]]) == 0:
                     del component[edge_to_break[0]]
-                    new_components.append(sc.parallelize([(edge_to_break[0], [])]))
+                    new_components.append(sc.parallelize([(edge_to_break[0], [])]).persist())
             if edge_to_break[1] in component.keys():
                 # print("Before removing - ", component[edge_to_break[1]])
                 component[edge_to_break[1]].remove(edge_to_break[0])
                 # print("After removing - ", component[edge_to_break[1]])
                 if len(component[edge_to_break[1]]) == 0:
                     del component[edge_to_break[1]]
-                    new_components.append(sc.parallelize([(edge_to_break[1], [])]))
-
+                    new_components.append(sc.parallelize([(edge_to_break[1], [])]).persist())
         # print("(get_network_components) num of Components after removing edge: ", len(old_network_components))
-
-        # performing bfs and getting all components.
-        # new_components = []
-        for component in old_network_components:
-            # perform bfs and keep adding RDDs of new components to new_components
-            vertices = set(component.keys())
-            visited = set()
-            remaining_vertices = set(component.keys())
-            while remaining_vertices:
-                bfsQ = [choice(tuple(remaining_vertices))]
-                new_component = [(bfsQ[0], component[bfsQ[0]])]
-                while bfsQ:
-                    node = bfsQ.pop(0)
-                    visited.add(node)
-                    children = component[node]
-                    for child in children:
-                        if child not in visited:
-                            visited.add(child)
-                            bfsQ.append(child)
-                            if (node, children) not in new_component:
-                                new_component.append((node, children))
-                            if (child, component[child]) not in new_component:
-                                new_component.append((child, component[child]))
-                new_components.append(sc.parallelize(new_component))
-                remaining_vertices = vertices.difference(visited)
-    # print_list = [x.collect() for x in new_components]
-    # print("(get_network_components) New components found from old comps: ", new_components)
+    for component in old_network_components:
+        # perform bfs and keep adding RDDs of new components to new_components
+        vertices = set(component.keys())
+        visited = set()
+        # new_components =[]
+        remaining_vertices = set(component.keys())
+        while remaining_vertices:
+            bfsQ = [choice(tuple(remaining_vertices))]
+            new_component = [(bfsQ[0], component[bfsQ[0]])]
+            while bfsQ:
+                node = bfsQ.pop(0)
+                visited.add(node)
+                children = component[node]
+                for child in children:
+                    if child not in visited:
+                        visited.add(child)
+                        bfsQ.append(child)
+                        if (node, children) not in new_component:
+                            new_component.append((node, children))
+                        if (child, component[child]) not in new_component:
+                            new_component.append((child, component[child]))
+            new_components.append(sc.parallelize(new_component).persist())
+            remaining_vertices = vertices.difference(visited)
     return new_components
 
 
@@ -183,7 +145,6 @@ def get_modularity(original_network, network_components_rdds, m):
     :param m: number of edges in original_network
     :return: modularity [-1, 1]
     '''
-    mod = 0
     network_components = []
     for rdd in network_components_rdds:
         network_components.append(rdd.collectAsMap())
@@ -201,7 +162,7 @@ def get_modularity(original_network, network_components_rdds, m):
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-power_file_rdd = sc.textFile("power_input.txt").map(lambda x: (x.split(" ")[0], x.split(" ")[1]))
+power_file_rdd = sc.textFile("sample.txt").map(lambda x: (x.split(" ")[0], x.split(" ")[1]))
 edges_rdd = power_file_rdd.flatMap(lambda x: [(x[0], x[1]), (x[1], x[0])]).distinct()
 
 m = edges_rdd.count() / 2
@@ -209,7 +170,6 @@ print("Number of edges = ", m)
 adj_list = edges_rdd.groupByKey().mapValues(lambda l: list(l))
 original_network = adj_list.collectAsMap()
 
-# Task 2 O/P 1
 betweenness_centrality = adj_list.flatMap(
     lambda node: get_btw_cent_compnt(node[0], original_network)).groupByKey().mapValues(
     lambda l: sum(l) / 2).collect()
@@ -217,88 +177,84 @@ betweenness_centrality = adj_list.flatMap(
 betweenness_centrality.sort(key=lambda x: x[0][0])
 betweenness_centrality.sort(key=lambda x: x[1], reverse=True)
 
-with open(str("task2a_xia.txt"), "w") as file:
+with open(str("task2a.txt"), "w") as file:
     for b in betweenness_centrality:
         file.write(str(b[0]) + ", " + str(b[1]) + "\n")
     file.close()
 
+
+# COMMUNITIES
+
 network_components = get_network_components([adj_list], None)
 final_communities = []
 
-# old_modularity = -1
 max_modularity = -1.0
 max_mod_network = tuple()
 mod_decreased_count = 0
 while True:
     edge_break_candidates = []
     # print("(While Loop) next Iter beginning network comps:", network_components)
+    while_start = time()
     print("(While Loop) Num. of components = ", len(network_components))
     num_nodes = 0
+    max_btw_cent = 0
+    edge_to_break = None
     for component in network_components:
         component_adj_list = component.collectAsMap()
-
         num_nodes += len(component_adj_list)
 
         if len(component_adj_list) == 1:
             continue
-
         # print("(While Loop) Component Len = ", len(component_adj_list))
         # print("Component = ")
         # for c in sorted(component_adj_list.items(), key=lambda x:x[0]):
         #   print(c)
         btw_cent = component.flatMap(
             lambda node: get_btw_cent_compnt(node[0], component_adj_list)).groupByKey().mapValues(
-            lambda l: sum(l) / 2).collect()
+            lambda l: sum(l) / 2).sortBy(lambda x: x[1]).collect()
         if btw_cent:
-            btw_cent.sort(key=lambda x: x[1], reverse=True)
-            edge_break_candidates.append(btw_cent[0])
+            if btw_cent[-1][1] > max_btw_cent:
+                edge_to_break = btw_cent[-1][0]
+            #btw_cent.sort(key=lambda x: x[1], reverse=True)
+            #edge_break_candidates.append(btw_cent[-1])
     # print("-------------------------")
     print("Sigma_nodes(components): ", num_nodes)
-    edge_to_break = tuple()
-    if edge_break_candidates:
-        edge_break_candidates.sort(key=lambda x: x[1], reverse=True)
-        edge_to_break = edge_break_candidates[0][0]
+    #edge_to_break = tuple()
+    #if edge_to_break:
+        #edge_break_candidates.sort(key=lambda x: x[1], reverse=True)
+        #edge_to_break = edge_break_candidates[0][0]
         # print("Edge to break - ", edge_to_break)
-    else:
-        print("No more edges to break. ")
-        edge_to_break = None
-
-    # have edge to break
-    # have network components
-    # calc new network components
+    #else:
+        #print("No more edges to break. ")
+        #edge_to_break = None
 
     new_network_components = get_network_components(network_components, edge_to_break)
     # print("(While Loop) New network components after breaking edge: ", new_network_components)
     # old_modularity = get_modularity(original_network, network_components, m)
-    new_modularity = get_modularity(original_network, new_network_components[:], m)
+    new_modularity = get_modularity(original_network, new_network_components, m)
     # print("Max modularity= ", max_modularity, "|", "New modularity= ", new_modularity)
 
     if new_modularity > max_modularity:
         max_mod_network = (new_modularity, new_network_components)
         max_modularity = new_modularity
         mod_decreased_count = 0
-        # old_modularity = new_modularity
-        # network_components = new_network_components[:]
-        # continue
     else:
         mod_decreased_count += 1
-        # final_communities = network_components[:]
-        # break
-    network_components = new_network_components[:]
+
+    network_components = new_network_components
 
     if len(network_components) == len(original_network): #mod_decreased_count > 25:
         break
+    print("While loop one iter: ", time() - while_start, "s")
+
 
 final_communities = max_mod_network[1]
-
 final_comm = [sorted(community.collectAsMap().keys()) for community in final_communities]
 final_comm.sort(key=lambda x: x[0])
 final_comm.sort(key=lambda x: len(x))
-
 print("Final Communities = ")
 for community in final_comm:
     print(len(community), community)
-
 with open(str("task2a_comm.txt"), "w") as file:
     for community_list in final_comm:
         if len(community_list) == 1:
@@ -310,7 +266,5 @@ with open(str("task2a_comm.txt"), "w") as file:
                 file.write(str("'" + community_list[num] + "'"))
             file.write("\n")
     file.close()
-
-print("Max Mod=", max_mod_network[0])
-# YAAAAY! Betweenness done!!!
+print("Max Mod =", max_mod_network[0])
 print("Time taken: ", time() - start_time, "s")
